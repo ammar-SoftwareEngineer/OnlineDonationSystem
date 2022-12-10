7969c19107366b986a656f3b6d70bb40aaba1787
realstate <aboamer0548@gmail.com>
2022-12-10 19:48:43 +0200

    new project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// let imgsign = document.querySelector(".login");
+// let logIn = document.getElementById("signUp");
+// let signUp = document.querySelector(".signup");
+
+// logIn.addEventListener("click", () => {
+  
+// });
