e69e2a184352f78b57462074f55a1ccb53e5c325
realstate <aboamer0548@gmail.com>
2022-12-11 02:05:21 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-// let imgsign = document.querySelector(".login");
-// let logIn = document.getElementById("signUp");
-// let signUp = document.querySelector(".signup");
+const signUp = document.getElementById("signUp");
+const signIn = document.getElementById("signIn");
+const signInBox = document.getElementById("signInBox");
+const signUpBox = document.getElementById("signUpBox");
 
-// logIn.addEventListener("click", () => {
+signUp.addEventListener("click", () => {
+  signInBox.classList.remove("slideIn");
+  signInBox.classList.add("slideOut");
   
-// });
+  setTimeout(() => {
+    signUpBox.classList.remove("slideOut");
+    signUpBox.classList.add("slideIn");
+  }, 300);
+});
+
+signIn.addEventListener("click", () => {
+  setTimeout(() => {
+    signInBox.classList.remove("slideOut");
+    signInBox.classList.add("slideIn");
+  }, 300);
+  signUpBox.classList.remove("slideIn");
+  signUpBox.classList.add("slideOut");
+});
