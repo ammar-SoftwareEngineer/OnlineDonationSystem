bd3cef4aaee2ee91adf3a06c3ed3f99d23994708
realstate <aboamer0548@gmail.com>
2022-12-13 06:57:46 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 (function ($) {
 "use strict";
 // TOP Menu Sticky
-$(window).on('scroll', function () {
-	var scroll = $(window).scrollTop();
-	if (scroll < 400) {
-    $("#sticky-header").removeClass("sticky");
-    $('#back-top').fadeIn(500);
-	} else {
-    $("#sticky-header").addClass("sticky");
-    $('#back-top').fadeIn(500);
-	}
-});
+// $(window).on('scroll', function () {
+// 	var scroll = $(window).scrollTop();
+// 	if (scroll < 400) {
+//     $("#sticky-header").removeClass("sticky");
+//     $('#back-top').fadeIn(500);
+// 	} else {
+//     $("#sticky-header").addClass("sticky");
+//     $('#back-top').fadeIn(500);
+// 	}
+// });
 
 
 $(document).ready(function(){
